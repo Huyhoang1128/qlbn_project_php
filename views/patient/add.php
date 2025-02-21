fa53d1a01d93a0cebd6bfce86e8f00f8a035d302
Pham Huy Hoang <hoangpham281103@gmail.com>
2025-02-21 14:17:04 +0700

    split css from view

--- views/patient/add.php
+++ views/patient/add.php
@@ -1,6 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,16 +6,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <style>
-        form{
-            width: 30%;
-            margin: auto;
-        }
-        button{
-            margin-top:10px;
-            width: 100%;
-        }
-    </style>
 </head>
 
 <body>
@@ -59,5 +46,3 @@
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
     </script>
 </body>
-
-</html>
