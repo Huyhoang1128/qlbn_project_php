50516adc84098813b57740a6653c2cd099c22382
Pham Huy Hoang <hoangpham281103@gmail.com>
2025-02-21 16:59:56 +0700

    rerouting

--- index.php
+++ index.php
@@ -1,34 +1,45 @@
 <?php
-    require_once('../app/config/config.php');
-    require_once APP_ROOT.'/app/libs/DbConnection.php';
+    require('config/config.php');
 
-    $controller = isset($_GET['controller'])?$_GET['controller']:'home';
-    $action = isset($_GET['action'])?$_GET['action']:'index';
-
-    if($controller == 'home'){
-        require_once APP_ROOT.'/app/controllers/HomeController.php';
-        $homeController = new HomeController();
-        $homeController->index();
-    } elseif ($controller == 'patient') {
-        require_once APP_ROOT.'/app/controllers/PatientController.php';
-        if($action=='create'){
-            $patientController = new PatientController();
-            $patientController->create();
+    if(isset($_GET['page'])){
+        $page = $_GET['page'];
+        if(isset($_GET['action'])){
+            $action=$_GET['action'];
+        }else{
+            $action= 'index';
         }
-        if($action=='delete'){
-            $id=$_GET['id'];    
-            $patientController = new PatientController();
-            $patientController->delete($id);
-        }
-        if($action=='update'){
-            $id=$_GET['id'];
-            $name=$_GET['name'];
-            $gender=$_GET['gender'];
-            $patientController = new PatientController();
-            $patientController->update($id,$name,$gender);
-        }
-    }else {
-        echo 'Nothing';
+    } else{
+        $page ='page';
+        $action='index';
     }
+    require "route.php";
+    // $controller = isset($_GET['controller'])?$_GET['controller']:'home';
+    // $action = isset($_GET['action'])?$_GET['action']:'index';
+
+    // if($controller == 'home'){
+    //     require_once APP_ROOT.'/app/controllers/HomeController.php';
+    //     $homeController = new HomeController();
+    //     $homeController->index();
+    // } elseif ($controller == 'patient') {
+    //     require_once APP_ROOT.'/app/controllers/PatientController.php';
+    //     if($action=='create'){
+    //         $patientController = new PatientController();
+    //         $patientController->create();
+    //     }
+    //     if($action=='delete'){
+    //         $id=$_GET['id'];    
+    //         $patientController = new PatientController();
+    //         $patientController->delete($id);
+    //     }
+    //     if($action=='update'){
+    //         $id=$_GET['id'];
+    //         $name=$_GET['name'];
+    //         $gender=$_GET['gender'];
+    //         $patientController = new PatientController();
+    //         $patientController->update($id,$name,$gender);
+    //     }
+    // }else {
+    //     echo 'Nothing';
+    // }
 
 ?>
